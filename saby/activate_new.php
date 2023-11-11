c8103632c3a5630544e0aea69ac34132cb85bd22
mohammadfaisalmaple <m.faisal@mapletele.com>
2023-11-11 16:37:49 +0300

    update

--- saby/activate_new.php
+++ saby/activate_new.php
@@ -5,62 +5,71 @@ require_once 'common.php';
 require_once 'Classes/auto.php';
 require_once 'Classes/Checker.php';
 
-
-
-
-myLog('Starting Saby..');
-$safeWidthPixels = 10;
+$activationFailed = false;
+myLog('Starting Saby Activation..');
+$safeWidthPixels = 50;
 // ARGV's As Received from the Starting Script and Kept in CAPITAL for Marking
-$PROFILE_AVD_NAME = $argv[1];
-myLog('Base AVD Name : ' . $PROFILE_AVD_NAME);
-sleep(2);
-$ENV_HOST_TYPE = $argv[2];
-myLog('Running Mode Type : ' . $ENV_HOST_TYPE);
-sleep(2);
-$ENV_HOST_NAME = $argv[3];
-myLog('Running Mode Name : ' . $ENV_HOST_NAME);
-sleep(2);
-$ENV_HOST_API = $argv[4];
+$ACTIVATION_SOURCE = $argv[1];
+myLog('AVD CREATED BY : ' . $ACTIVATION_SOURCE);
+
+$ENV_HOST_API = $argv[2];
 myLog('API Manager Url : ' . $ENV_HOST_API);
 $ENV_HOST_API = 'http://' . $ENV_HOST_API;
 myLog('API Manager Full Url : ' . $ENV_HOST_API);
-sleep(2);
-$ENV_HOST_ASTERISK = $argv[5];
-myLog('Asterisk Manager Details : ' . $ENV_HOST_ASTERISK);
-sleep(2);
-$ENV_HOST_BEANSTALK = $argv[6];
-myLog('Beanstalk Manager Url : ' . $ENV_HOST_BEANSTALK);
-myLog('Beanstalk Manager Full Url : ' . $ENV_HOST_BEANSTALK);
-sleep(2);
-$EMULATOR_ALPHA = (strlen($argv[7]) != 1 ? 'A' : $argv[7]);
-myLog('Saby Emulator Alpha : ' . $EMULATOR_ALPHA);
-sleep(2);
-$EMULATOR_ID = $argv[8];
-myLog('Saby Emulator Suffix ID : ' . $EMULATOR_ID);
-sleep(2);
-$RUNNING_EMULATOR_PORT = '5544';
-myLog('Saby Emulator Port : ' . $RUNNING_EMULATOR_PORT);
-sleep(2);
-$RUNNING_EMULATOR_X = $argv[10];
-myLog('Saby Emulator X posision : ' . $RUNNING_EMULATOR_X);
-sleep(2);
-$RUNNING_EMULATOR_Y = $argv[11];
-myLog('Saby Emulator Y posision : ' . $RUNNING_EMULATOR_Y);
-sleep(2);
-$RUNNING_EMULATOR_WIDTH = $argv[12] + $safeWidthPixels;
-myLog('Saby Emulator Width : ' . $RUNNING_EMULATOR_WIDTH);
-sleep(2);
-$RUNNING_EMULATOR_HEIGHT = $argv[13];
-myLog('Saby Emulator Height : ' . $RUNNING_EMULATOR_HEIGHT);
-sleep(2);
-$PROFILE_ANDROID = $argv[14];
-myLog('Saby Emulator Android : ' . $PROFILE_ANDROID);
-sleep(2);
-$PROFILE_WA_TYPE = $argv[15];
+$conf['api_server'] = $ENV_HOST_API;
+$PROFILE_WA_TYPE    = $argv[3];
 myLog('Saby Emulator Application Short Name: ' . $PROFILE_WA_TYPE);
 $PROFILE_WA_TYPE = "whatsapp";
 myLog('Saby Emulator Application Full Name: ' . $PROFILE_WA_TYPE);
 
+$PROFILE_ANDROID = $argv[4];
+myLog('Saby Emulator Android : ' . $PROFILE_ANDROID);
+
+$PROFILE_AVD_NAME = $argv[5];
+myLog('Base AVD Name : ' . $PROFILE_AVD_NAME);
+
+$VPN_PROVIDER = $argv[6];
+myLog('VPN APP : ' . $VPN_PROVIDER);
+
+$VPN_REGION = $argv[7];
+myLog('VPN TARGET REGION : ' . $VPN_REGION);
+
+$SIM_PROVIDER = $argv[8];
+myLog('SIM NUMBER SOURCE : ' . $SIM_PROVIDER);
+
+$SIM_REGION = $argv[9];
+myLog('SIM NUMBER COUNTRY ID : ' . $SIM_REGION);
+
+if (!is_numeric($SIM_REGION) && ($SIM_PROVIDER == '5S' || $SIM_PROVIDER == 'SmsActivate')) {
+    $SIM_REGION = getCountryByName($VPN_PROVIDER, $SIM_PROVIDER, $SIM_REGION);
+    myLog('SIM NUMBER COUNTRY ID CORRECTION: ' . $SIM_REGION);
+}
+
+$ACTIVATION_TYPE = $argv[10];
+myLog('THIS ACTIVATION SCENARO IS : ' . $ACTIVATION_TYPE);
+
+$EMULATOR_ALPHA = $argv[11];
+myLog('Saby Emulator Alpha : ' . $EMULATOR_ALPHA);
+
+$EMULATOR_ID = $argv[12];
+myLog('Saby Emulator Suffix ID : ' . $EMULATOR_ID);
+
+$RUNNING_EMULATOR_PORT = $argv[13];
+myLog('Saby Emulator Port : ' . $RUNNING_EMULATOR_PORT);
+
+$RUNNING_EMULATOR_X = $argv[14];
+myLog('Saby Emulator X posision : ' . $RUNNING_EMULATOR_X);
+
+$RUNNING_EMULATOR_Y = $argv[15];
+myLog('Saby Emulator Y posision : ' . $RUNNING_EMULATOR_Y);
+
+$RUNNING_EMULATOR_WIDTH = $argv[16] + $safeWidthPixels;
+myLog('Saby Emulator Width : ' . $RUNNING_EMULATOR_WIDTH);
+
+$RUNNING_EMULATOR_HEIGHT = $argv[17];
+myLog('Saby Emulator Height : ' . $RUNNING_EMULATOR_HEIGHT);
+$ENV_IP = $argv[18];
+myLog('M3allem IP : ' . $ENV_IP);
 
 $sabyName = getSabyName();
 myLog("Saby Name : " . $sabyName);
@@ -85,6 +94,22 @@ $auto= new Automate($sabyName, $sabyFullName, $RUNNING_EMULATOR_PORT);
 myLog('New Region : ' . $RUNNING_EMULATOR_X . ',' . $RUNNING_EMULATOR_Y . ',' . $RUNNING_EMULATOR_WIDTH . ',' . $RUNNING_EMULATOR_HEIGHT);
 $auto->pythonUI->setDefaultRegion($RUNNING_EMULATOR_X, $RUNNING_EMULATOR_Y, $RUNNING_EMULATOR_WIDTH, $RUNNING_EMULATOR_HEIGHT);
 myLog("Automate loading time: " . sprintf('%.2f', microtime(true) - $autoStartTime));
+
+
+$transporter          = new Transporter_New_Ma3llem($sabyName, $sabyFullName, $RUNNING_EMULATOR_PORT, $androidID, $ENV_HOST_API, $ENV_IP);
+
+
+$transporter->setAndroidID($androidID);
+switch ($VPN_PROVIDER) {
+        case "HTZ-express":
+            $transporter->updateWireXpressVpnConfig($EMULATOR_ALPHA, 60, 2);
+            $checker->rebootAVD();
+            $auto->ConnectWireGuard($PROFILE_ANDROID, 'New');
+            break;
+        default:
+            $activationFailed = true;
+            goto activationFailed;
+}
 
 
 myLog("Install IP Location APP");
