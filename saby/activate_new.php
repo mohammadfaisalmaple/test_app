ea5a0b54f7ee11ac770fe61425e099ce591845f2
mohammadfaisalmaple <m.faisal@mapletele.com>
2023-11-11 16:42:50 +0300

    update

--- saby/activate_new.php
+++ saby/activate_new.php
@@ -54,7 +54,7 @@ myLog('Saby Emulator Alpha : ' . $EMULATOR_ALPHA);
 $EMULATOR_ID = $argv[12];
 myLog('Saby Emulator Suffix ID : ' . $EMULATOR_ID);
 
-$RUNNING_EMULATOR_PORT = $argv[13];
+$RUNNING_EMULATOR_PORT = '5544';
 myLog('Saby Emulator Port : ' . $RUNNING_EMULATOR_PORT);
 
 $RUNNING_EMULATOR_X = $argv[14];
